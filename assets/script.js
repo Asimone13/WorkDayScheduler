bb9713504a164741108779d626da00adf34f4092
Alec Simone <alecsimone2@gmail.com>
2022-10-24 10:05:06 -0400

    save button click listener

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,7 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+$(document).ready(function () {r 
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
